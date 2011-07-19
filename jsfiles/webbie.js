1701658a665f27a4446ea7f29a1383a18b540c8c
Josh Hoak <jrhoak@gmail.com>
2011-07-19 10:25:10 -0700

    webbie

--- jsfiles/webbie.js
+++ jsfiles/webbie.js
@@ -9,17 +9,10 @@ var webload = (function() {
 
   var loadContent = function(location) {
     $("#mainbox").load(location, function() { createButtons("mainbox", "jlink"); });
-    /*
-    $.get(location, function(data) {
-      $("#mainbox").empty();
-      $("#mainbox").append(data);
-      createButtons("mainbox", "jlink");
-    });
-    */
   };
 
   var createButtons = function(id, classname) {
-    $("#"+id).find(classname).each(function(i){ 
+    $("#"+id).find(classname).each(function(i) { 
       var that = this;
       $(this).click(function(){ loadContent(that.getAttribute("location")); });
     });
