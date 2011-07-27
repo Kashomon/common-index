3e7119d1e1f9f8ccd9e777909a67665299ac5f97
Josh Hoak <jrhoak@gmail.com>
2011-07-26 19:13:35 -0700

    webbie chances

--- jsfiles/webbie.js
+++ jsfiles/webbie.js
@@ -1,5 +1,7 @@
 var webload = (function() {
 
+  var webcontent = {};
+
   var loadWebpage = function() {
     $(document).ready( function() {
       createButtons("newlinklist", ".jtoplink");
@@ -8,13 +10,31 @@ var webload = (function() {
   };
 
   var loadContent = function(location) {
-    $("#mainbox").load(location, function() { createButtons("mainbox", "jlink"); });
+    $("#mainbox").empty();
+    if (webcontent[location] === undefined) {
+      $.get(location, function(data) {
+        webcontent[location] = data; 
+        replaceContent(data);
+      }); 
+    } else {
+      replaceContent(webcontent[location]);
+    }
   };
+
+  var replaceContent = function(content) {
+    $("#mainbox").empty();
+    $("#mainbox").append(content);
+    createButtons("mainbox", "jlink");
+  }
 
   var createButtons = function(id, classname) {
     $("#"+id).find(classname).each(function(i) { 
       var that = this;
       $(this).click(function(){ loadContent(that.getAttribute("location")); });
+      $(this).hover(
+        function(){ $(that).css("color", "#880000"); },
+        function(){ $(that).css("color", "#CC4500"); }
+      )
     });
   };
 
